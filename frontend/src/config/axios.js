6c25d59862ce8a9c25b0040dc253f3b9c93a19cc
AyushSrivastava729 <ayushsrivastava729@gmail.com>
2025-11-13 00:33:30 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://realtime-chat-app-with-google-gemini.onrender.com",
 });
 
 // Optional: Set Authorization header dynamically before each request
